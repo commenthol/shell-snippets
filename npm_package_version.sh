f30dd445058edc8e32f35b91c321d89cbb38c824
commenthol <commenthol@gmail.com>
2022-02-23 22:26:21 +0100

    fix(npm_package_version): only use first match

--- npm_package_version.sh
+++ npm_package_version.sh
@@ -1,5 +1,5 @@
 #----
 # get the version from a package.json file with sh
 npm_package_version() {
-	grep '"version":' | cut -d'"' -f4 | tr -d "[:space:]"
+	grep '"version":' | head -n 1 | cut -d'"' -f4 | tr -d "[:space:]"
 }
