c1e2861818bb09d79002f38c31eb4e95c9ade287
commenthol <commenthol@gmail.com>
2022-02-25 18:58:17 +0100

    docs: minor remarks

--- dotenv.sh
+++ dotenv.sh
@@ -1,6 +1,7 @@
 #----
 # read a .env file and export all variables to work in the current shell
 # execute the command in with copy & paste
+# NOTE: You'll have to start the script with `.` or `source`
 dotenv () {
 	# export $(cat .env | sed 's/#.*//g' | xargs)
 	## ubi does not include xargs
--- has_cmd.sh
+++ has_cmd.sh
@@ -1,6 +1,7 @@
 
 #----
 # check if command is available on the system
+# 0 = true; 1 = false
 has_cmd () {
 	type $1 > /dev/null 2>&1
 	echo $?
